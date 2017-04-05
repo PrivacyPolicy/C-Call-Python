7ff2b05211caa784afbaef8bfd48036b3239ab74
PrivacyPolicy <Gabehwebsites@gmail.com>
2017-04-04 21:21:17 -0400

    Code in place, but no module yet

--- commands.sh
+++ commands.sh
@@ -3,5 +3,5 @@ sudo apt-get install libpython-all-dev # C binaries
 
 # Compile
 # old: gcc -I/usr/include/python2.7 CallPythonTest.c -lpython2.7
-gcc CallPythonTest.c -lpython2.7 -o CallPythonTest
+gcc CallPythonTest.c -lpython2.7 -I/usr/include/python2.7 -o CallPythonTest
 
