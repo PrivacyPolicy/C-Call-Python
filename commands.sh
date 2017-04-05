b4cd143aead7a2100026b172ace5631b6c1a90da
PrivacyPolicy <Gabehwebsites@gmail.com>
2017-04-04 20:53:40 -0400

    Added more details to commands list

--- commands.sh
+++ commands.sh
@@ -1,3 +1,6 @@
+sudo apt-get install python-dev # 2.7
+sudo apt-get install libpython-all-dev # C binaries
+
 # Compile
 # old: gcc -I/usr/include/python2.7 CallPythonTest.c -lpython2.7
 gcc CallPythonTest.c -lpython2.7 -o CallPythonTest
