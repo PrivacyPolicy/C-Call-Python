39e5bd0d78ec9027ef156b0907c26a0c235b18be
PrivacyPolicy <Gabehwebsites@gmail.com>
2017-04-05 16:14:31 -0400

    Completely working (just with lots of junk)

--- CallPythonSentiment.c
+++ CallPythonSentiment.c
@@ -1,44 +1,63 @@
 #include <Python.h>
 
+float getSentiment();
+
 int main(int argc, char* argv[])
 {
-  PyObject *pName, *pModule, *pDict, *pFunc;
-  PyObject /**pArgs, */*pValue;
-  int i;
-  
   if (argc < 2) {
-    fprintf(stderr, "Usage: pass \"your message like this\"\n");
-    return 1;
+    fprintf(stderr, "Usage: call this file with a sentence in quotes to analyze.\n");
   }
+  fprintf(stdout, "sentiment: %f\n", getSentiment(argv[1]));
+}
+
+float getSentiment(char* sentence)
+{
+  PyObject *pName, *pModule, *pDict, *pFunc;
+  PyObject *pArgs, *pValue;
+  int i;
   
   Py_Initialize();
   // Refer to local file, not python module
   PyRun_SimpleString("import sys");
   PyRun_SimpleString("sys.path.append(\".\")");
-  
-  pName = PyString_FromString("sentiment"); // file that we're importing
-  
+  PyRun_SimpleString("from sentimentweb.info import feature_selection_trials, MyDict, classify2");
+
+  pName = PyString_FromString("sentiment");
+  // error checking should occur
+
   pModule = PyImport_Import(pName);
   Py_DECREF(pName);
-  
+
   if (pModule != NULL) {
     pFunc = PyObject_GetAttrString(pModule, "analyze");
-    
+    // pFunc is a new reference
+
     if (pFunc && PyCallable_Check(pFunc)) {
-      //pArgs = PyTuple_New(argc - 2);
-      pValue = PyString_FromString(argv[1]);
+      pArgs = PyTuple_New(1);
+      pValue = PyString_FromString(sentence);
       if (!pValue) {
-        //Py_DECREF(pArgs);
+        Py_DECREF(pArgs);
         Py_DECREF(pModule);
-        fprintf(stderr, "cannot convert arugment");
+        fprintf(stderr, "Cannot convert argument\n");
         return 1;
       }
-      pValue = PyObject_CallObject(pFunc, pValue);
-      Py_DECREF(pValue);
+      // pValue reference stolen here:
+      PyTuple_SetItem(pArgs, 0, pValue);
+      pValue = PyObject_CallObject(pFunc, pArgs);
+      Py_DECREF(pArgs);
       if (pValue != NULL) {
-        printf("Result: %s\n", PyString_AsString(pValue));
+        int positive = PyTuple_GetItem(pValue, 0) == Py_True;
+        double probability = PyFloat_AsDouble(PyTuple_GetItem(pValue, 1));
+        //printf("Result of call: %o\n", PyTuple_AsTuple(pValue));
+        printf("Sentiment is: %s. Probability is: %0.3f.\n",
+            (positive) ? "positive" : "negative", probability);
+        double d = ((positive) ? 1 : -1) * probability;
+        if (d > 1) d = 1;
+        if (d < -1) d = -1;
         Py_DECREF(pValue);
-      } else {
+        return d;
+      }
+      else {
         Py_DECREF(pFunc);
         Py_DECREF(pModule);
         PyErr_Print();
@@ -46,16 +65,17 @@ int main(int argc, char* argv[])
         return 1;
       }
     } else {
-      if (PyErr_Occurred()) PyErr_Print();
-      fprintf(stderr, "Cannot find function analyze\n");
+      if (PyErr_Occurred())
+        PyErr_Print();
+      fprintf(stderr, "Cannot find function analyze.\n");
     }
     Py_XDECREF(pFunc);
     Py_DECREF(pModule);
   } else {
     PyErr_Print();
-    fprintf(stderr, "Failed to load 'sentiment.py'\n");
+    fprintf(stderr, "Failed to comprehend \"%s\", I guess.\n", sentence);
     return 1;
   }
   Py_Finalize();
   return 0;
-}    
+}
--- sentiment.py
+++ sentiment.py
@@ -5,18 +5,24 @@
 
 from sentimentweb.info import feature_selection_trials, MyDict, classify2
 
-text = 'Good afternoon. My name is Russel, and I\'m a senior wilderness explorer. Are you in need of any assistance today, Sir?'
-text2 = 'I hate this person with a passion, so I couldn\'t enjoy the movie'
+#text = 'Good afternoon. My name is Russel, and I\'m a senior wilderness explorer. Are you in need of any assistance today, Sir?'
+#text2 = 'I hate this person with a passion, so I couldn\'t enjoy the movie'
 
-def analyze(text3):
+def analyze(text):
     #from sentimentweb.info import feature_selection_trials
     #from sentimentweb.info import MyDict
     #t = MyDict()
     #print(t)
-    print(feature_selection_trials())
-    print(classify2(text))
-    print(classify2(text2))
-    print(classify2(text3))
+    feature_selection_trials()
+    #print(classify2(text))
+    #print(classify2(text2))
+    #print(classify2(text3))
+    #value = classify2(text3)
+    #return {
+    #    'sentiment': 'positive' if value[0] else 'negative',
+    #    'certainty': value[1]
+    #}
+    return classify2(text)
 
 #def main():
 #    analyze("Hi there, my name is a name of happy happy happy.")
